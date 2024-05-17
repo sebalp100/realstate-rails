f226241d905004cbfc2d28af4969278fffa93033
sebalp100 <sebalp100@gmail.com>
2024-05-16 22:21:53 -0300

    Fix navbar and search to be responsive

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,8 +3,17 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 
 document.addEventListener('DOMContentLoaded', () => {
-  const button = document.querySelector('button');
-  button.addEventListener('click', () => {
-    alert('Button clicked!');
-  });
+  const toggleButton = document.getElementById('toggleButton');
+  const overlay = document.getElementById('overlay');
+  const mobileMenu = document.getElementById('mobileMenu');
+  const closeButton = document.getElementById('closeButton');
+
+  function toggleMenu() {
+    overlay.classList.toggle('hidden');
+    mobileMenu.classList.toggle('hidden');
+  }
+
+  toggleButton.addEventListener('click', toggleMenu);
+  overlay.addEventListener('click', toggleMenu);
+  closeButton.addEventListener('click', toggleMenu);
 });
